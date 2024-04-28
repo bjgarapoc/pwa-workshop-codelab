5367a2724dc342078037e3e6fc43e7b567b88f41
aebr3 <beatricearapoc@gmail.com>
2024-04-28 21:17:00 +0800

    seventh section - done

--- js/preview.js
+++ js/preview.js
@@ -13,14 +13,3 @@
  See the License for the specific language governing permissions and
  limitations under the License.
  */
-
-import { openDB } from 'idb';
-import { marked } from 'marked';
-
-window.addEventListener('DOMContentLoaded', async () => {
-  const preview = document.querySelector('.preview');
-  const db = await openDB('settings-store');
-  const content = (await db.get('settings', 'content')) || '';
-
-  preview.innerHTML = marked(content);
-});
